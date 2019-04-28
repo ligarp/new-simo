320be431bf137f63f56c669f6e33450756e41f31
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2019-04-28 17:08:18 +0700

    Fix terakhir

--- development/dummy_publish.py
+++ development/dummy_publish.py
@@ -8,4 +8,4 @@ client1= paho.Client("control1")                           #create client object
 client1.on_publish = on_publish                          #assign function to callback
 client1.connect(broker,port)                                 #establish connection
 client1.username_pw_set('ragil', 'ragil')
-ret= client1.publish("lab/smoke",500)                   #publish
+ret= client1.publish("lab/temperature",20)                   #publish
--- development/mqtt.py
+++ development/mqtt.py
@@ -11,7 +11,7 @@ server = "192.168.100.9"
 client_name = str(datetime.datetime.now())
 client = mqtt.Client("client-"+client_name)
 client.username_pw_set(username,password)
-tTemperature = datetime.datetime.now() + timedelta(minutes=10)
+tTemperature = datetime.datetime.now() - timedelta(minutes=10)
 tSmoke = datetime.datetime.now() - timedelta(minutes=10)
 tDefault = datetime.datetime.now() - timedelta(minutes=10)
 tDefault1 = datetime.datetime.now() - timedelta(minutes=10)
@@ -27,11 +27,14 @@ def on_connect(client, userdata, rc):
         print ("connected" + str(rc))
 def on_message(client, userdata, msg):
         global tDefault,tDefault1,tDefault2,tTemperature,tSmoke, tAlertSmoke,tAlertTemperature,delayAlert
+        print(str(msg.topic))
+        # print("Message :" + str(msg.topic.decode("utf-8")))
         # print("Message :" + str(msg.payload.decode("utf-8")))
         # print ("client"+str(datetime.datetime.now()))
         if(str(msg.topic)=="lab/temperatureTes"):
                 value = str(msg.payload.decode("utf-8"))
                 print (tAlertTemperature)
+                
 
                 if ((datetime.datetime.now()-tAlertTemperature)>=delayAlert and int(value) > 30) :
                         function.post("temperature",value)
@@ -41,6 +44,7 @@ def on_message(client, userdata, msg):
         #dummy
         if(str(msg.topic)=="lab/pir"):                
                 value = msg.payload.decode("utf-8")
+                print(value)
                 if ((datetime.datetime.now()-tDefault)>=delayUpdate) :
                         function.post("pir",value)
                         tDefault = datetime.datetime.now()
@@ -49,6 +53,8 @@ def on_message(client, userdata, msg):
 
         if(str(msg.topic)=="lab/temperature"):
                 value = str(msg.payload.decode("utf-8"))
+                print(value)
+                print(tTemperature)
                 if ((datetime.datetime.now()-tAlertTemperature)>=delayAlert and int(value) > 32) :
                         function.post("temperature",value)
                         smsgateway.kirim_pesan('082220488112','Peringatan !! Suhu berbahaya. '+value+ '° Celcius')
@@ -64,6 +70,7 @@ def on_message(client, userdata, msg):
         #         # function.post("smoke",str(msg.payload.decode("utf-8")))
         #         # function.put("smoke",str(msg.payload.decode("utf-8")))
                 value = str(msg.payload.decode("utf-8"))
+                print(value)
                 if ((datetime.datetime.now()-tAlertSmoke)>=delayAlert) and int(value)>403 :
                         function.post("smoke",value)
                         smsgateway.kirim_pesan("082220488112",'Tingkat asap berbahaya.')
@@ -76,6 +83,7 @@ def on_message(client, userdata, msg):
 
         if(str(msg.topic)=="lab/current"):
                 value = str(msg.payload.decode("utf-8"))
+                print(value)
                 if ((datetime.datetime.now()-tDefault1)>=delayUpdate) :
                         function.post("current",value)
                         tDefault1 = datetime.datetime.now()
@@ -83,6 +91,7 @@ def on_message(client, userdata, msg):
         if(str(msg.topic)=="lab/ldr"):
         #         print(type(msg.payload.decode("utf-8")))
                 value = int(msg.payload.decode("utf-8"))
+                print(value)
                 if ((datetime.datetime.now()-tDefault2)>=delayUpdate):
                         function.post("ldr",value)
                         tDefault2 = datetime.datetime.now()
